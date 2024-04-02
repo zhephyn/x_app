aa2c66123d924c9817a8da8dead2644ca30990b2
zhephyn <zhephynwinter@gmail.com>
2024-04-02 15:51:36 +0300

    completed the MVP for creating and only editing or destroying tweet by the user who created it

--- app/controllers/TweetsController.rb
+++ app/controllers/TweetsController.rb
@@ -6,14 +6,24 @@ class TweetsController < ApplicationController
     end
 
     def index
+        if user_signed_in?
+            @tweets = Tweet.all
+        else
+            #render a signin or registration form for them to create an account
+        end 
         #should be nicknamed profile, use devise helper called current_user, current_user should be used with other actions too
-        @user = current_user
-        @tweets = @user.tweets
+        #@user = current_user
+        #@tweets = @user.tweets
     end
-
     def show
-        @user = current_user
-        @tweet = @user.tweets.find(params[:id])
+        if user_signed_in?
+            @tweet = Tweet.find(params[:id])
+        else
+            ##render a page for the user to sign in or register first
+        end
+        #the lines below are commented because any on should be able to see the tweet regardless if they created it or not
+        #@user = current_user
+        #@tweet = @user.tweets.find(params[:id])
     end
 
     def new
@@ -29,25 +39,40 @@ class TweetsController < ApplicationController
         @user = current_user
         @tweet = @user.tweets.create(tweet_params)
         @tweet.save
+        redirect_to tweet_path(@tweet)
     end 
 
     def edit
-        @user = current_user
-        @tweet = @user.tweets.find(params[:id])
-        @updated_tweet = @tweet.update
+        #find tweet
+        #check to see if the user who wants to edit is the same user that made the tweet
+        #edit the tweet
+        @tweet = Tweet.find(params[:id])
+        if current_user.id == @tweet.user_id
+            render :edit
+        else
+            redirect_to tweets_path, alert: "You can't edit this tweet since you didnot create it"
+        end
     end
 
     def update
-        @user = current_user
-        @tweet = @user.tweets.find(params[:id])
-        @updated_tweet = @tweet.update(tweet_params)
-        @updated_tweet.save
+        @tweet = Tweet.find(params[:id])
+        if current_user.id == @tweet.user_id   
+            @tweet.update(tweet_params)
+            @tweet.save
+            redirect_to tweet_path(@tweet), notice: "Tweet is successfully updated"
+        else
+            redirect_to :root, alert: "You can't edit a tweet you didnt create"
+        end
     end
 
     def destroy
-        @user = current_user
-        @tweet = @user.tweets.find(params[:id])
-        @tweet.destroy
+        @tweet = Tweet.find(params[:id])
+        if current_user.id == @tweet.user_id
+            @tweet.destroy
+            redirect_to tweets_path
+        else
+            redirect_to :root
+        end
     end
 
     private
