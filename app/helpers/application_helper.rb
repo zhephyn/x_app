49810184d184057bfc962aeda9f74ee38718294a
zhephyn <zhephynwinter@gmail.com>
2024-06-04 08:43:34 +0300

    final commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
     def display_side_bars?
       !(controller_name == "tweets" && action_name == "home")
+      !(controller_name == "sessions" && action_name == "new")
     end
 end
